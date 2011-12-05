8a98f12c087e7de51b574cdb3db47df51042aa44
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2011-12-05 13:18:28 -0500

    Start work on Database class

---
+++ MySQL/Database.php
@@ -0,0 +1,78 @@
+<?php
+require_once("../Common/common.php");
+require_once(BPATH . "/Common/ExecuteHandler.php");
+require_once(BPATH . "/MySQL/Database.settings.php");
+class Database extends ExecuteHandler {
+	private $err = false;
+	private $table; // table we're currently working on
+	private $columns; // columns in that table
+	private $verbose = true;
+	public $errmessage = '';
+	public function __construct() {
+		parent::__construct(self::$Database_commands);
+		if(mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) === false) {
+			$this->err = true;
+			$this->errmessage = mysql_error();
+			return;
+		}
+		$this->query("USE " . mysql_real_escape_string(DB_NAME));
+	}
+	protected static $Database_commands = array(
+		'count' => array('name' => 'count',
+			'desc' => 'Count from a table',
+			'arg' => 'None',
+			'execute' => 'callmethod'),
+		'settable' => array('name' => 'settable',
+			'desc' => 'Set the default table to work with',
+			'arg' => 'Table name',
+			'execute' => 'callmethodarg'),
+	);
+	public function cli() {
+		return $this->setup_commandline('Database');
+	}
+	private function query($sql) {
+		$result = mysql_query($sql);
+		if($result == false) {
+			$this->err = true;
+			$this->errmessage = mysql_error();
+			if($this->verbose) {
+				echo 'Query failed: ' . $sql . PHP_EOL;
+				echo 'Error message: ' . $this->errmessage . PHP_EOL;
+			}
+		}
+		else {
+			$this->err = false;
+		}
+		return $result;
+	}
+	public function settable($table) {
+		$table = mysql_real_escape_string($table);
+		$columns = $this->query("SHOW COLUMNS FROM " . $table);
+		if(!$columns) {
+			echo 'Failed to set table' . PHP_EOL;
+			return false;
+		}
+		$this->table = $table;
+		$this->columns = array();
+		while($column = mysql_fetch_array($columns)) {
+			$this->columns[] = $column['Field'];
+		}
+		return true;
+	}
+	public function count($paras = array()) {
+		$query = 'SELECT COUNT(*) FROM ' . $this->table . ' ' . $this->where($paras);
+	}
+	private function where($paras) {
+	// assemble where clause
+		
+	}
+	public function showcolumns() {
+	// show columns in default table
+		if(!$this->table) return false;
+		echo 'Columns in table ' . $this->table . PHP_EOL;
+		foreach($this->columns as $column) {
+			echo $column . PHP_EOL;
+		}
+		return true;
+	}
+}

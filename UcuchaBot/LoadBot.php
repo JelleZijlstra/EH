602ef066e1604b827c023dc3c189ea4263926667
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2011-12-09 23:50:22 -0500

    comment LoadBot.php

--- UcuchaBot/LoadBot.php
+++ UcuchaBot/LoadBot.php
@@ -3,11 +3,14 @@
 require_once('../Common/common.php');
 require_once(BPATH . '/UcuchaBot/Bot.php');
 $bot = new Bot();
+// remove script name
 array_shift($argv);
+// get command to be executed
 $cmd = implode($argv, ' ');
 if($cmd) {
 	$bot->execute($cmd);
 	$bot->execute('quit');
 }
-else
+else {
 	$bot->cli();
+}

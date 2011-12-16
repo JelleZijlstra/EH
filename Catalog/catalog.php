dcdc4c5a5f76acc348f712d81846e6f26d3156d2
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2011-12-16 18:50:47 +0100

    Remove things that are now better handled by command execution

--- Catalog/catalog.php
+++ Catalog/catalog.php
@@ -7,33 +7,9 @@ if(!in_array($argv[1], array('backup', 'revert', 'diff', 'findcode', 'test')))
 else
 	require_once(__DIR__ . '/settings.php');
 switch($argv[1]) {
-	case '': case 'byfile': $csvlist->cli(); break; // perform by-file operations
-	case 'check': $csvlist->check(); break; // check for new files, add them to catalog
-	case 'format': // format catalog
-		if($argv[2] === '-w')
-			$csvlist->formatall(true); 
-		else
-			$csvlist->formatall();
-		break;
-	case 'adddata': // add data to catalog entries
-	case 'edittitle': $csvlist->doall($argv[1]); break; // edit titles
-	// find files with a certain key-value pair
-	// usage: ./catalog.php find KEY VALUE
-	case 'find':
-		if($argv[3]) $csvlist->find($argv[2], $argv[3]);
-		else $csvlist->find_input($argv[2]);
-		break;
-	case 'dupes': $csvlist->dups(); break;
-	case 'stats': $csvlist->stats(); break;
-	// list values
-	case 'list':
-		$paras = array();
-		if($argv[3]) $paras['sort'] = $argv[3];
-		if($n3 = in_array($argv[3], array('true', 'false')) or $n4 = $argv[4]) {
-			$v = $n3 ? $n3 : $n4;
-			$paras['isfunc'] = $v;
-		}
-		$csvlist->mlist($argv[2], $paras);
+	case '': case 'byfile': 
+		// enter main CLI
+		$csvlist->cli(); 
 		break;
 	case 'backup': // backup catalog
 		catalog_backup();

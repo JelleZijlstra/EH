e69efec8eeb97ed81d2186cf122653647bd83a1b
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2011-12-21 12:40:26 +0100

    Fix notices

--- Catalog/catalog.php
+++ Catalog/catalog.php
@@ -2,12 +2,14 @@
 <?
 require_once(__DIR__ . '/../Common/common.php');
 // don't load catalog when we have an argument that doesn't need it
-if(!in_array($argv[1], array('backup', 'revert', 'diff', 'findcode', 'test')))
+if(!isset($argv[1]) or !in_array($argv[1], array('backup', 'revert', 'diff', 'findcode', 'test')))
 	require_once(BPATH . '/Catalog/load.php');
 else
 	require_once(BPATH . '/Catalog/settings.php');
-switch($argv[1]) {
-	case '': case 'byfile': 
+if(!isset($argv[1]))
+	$csvlist->cli();
+else switch($argv[1]) {
+	case 'byfile': 
 		// enter main CLI
 		$csvlist->cli(); 
 		break;

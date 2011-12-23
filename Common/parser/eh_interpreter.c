e23b137f225c18869c22609b31574f4d27ffb0c9
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2011-12-23 22:27:49 +0100

    Add separate files

---
+++ Common/parser/eh_interpreter.c
@@ -0,0 +1,72 @@
+#include "eh.h"
+
+int execute(ehnode_t *node) {
+	if(node == NULL)
+		return 0;
+	//printf("Executing nodetype %d\n", node->type);
+	switch(node->type) {
+		case idnode_enum:
+			return node->id.name;
+		case connode_enum:
+			return node->con.value;
+		case opnode_enum:
+			//printf("Executing opcode: %d\n", node->op.op);
+			switch(node->op.op) {
+				case T_ECHO:
+					switch(node->op.paras[0]->type) {
+						case idnode_enum:
+							printf("%s\n", node->op.paras[0]->id.name);
+							return 0;
+						case connode_enum:
+							printf("%d\n", node->op.paras[0]->con.value);
+							return 0;
+						case opnode_enum:
+							printf("%d\n", execute(node->op.paras[0]));
+					}
+					
+					return 0;
+				case T_IF:
+					if(execute(node->op.paras[0]))
+						execute(node->op.paras[1]);
+					return 0;
+				case T_SEPARATOR:
+					execute(node->op.paras[0]);
+					execute(node->op.paras[1]);
+					return 0;
+				case '=':
+					return execute(node->op.paras[0]) == 
+						execute(node->op.paras[1]);
+				case '>':
+					return execute(node->op.paras[0]) >
+						execute(node->op.paras[1]);
+				case '<':
+					return execute(node->op.paras[0]) <
+						execute(node->op.paras[1]);
+				case T_GE:
+					return execute(node->op.paras[0]) >= 
+						execute(node->op.paras[1]);
+				case T_LE:
+					return execute(node->op.paras[0]) <=
+						execute(node->op.paras[1]);
+				case T_NE:
+					return execute(node->op.paras[0]) != 
+						execute(node->op.paras[1]);
+				case '+':
+					return execute(node->op.paras[0]) + 
+						execute(node->op.paras[1]);
+				case '-':
+					return execute(node->op.paras[0]) - 
+						execute(node->op.paras[1]);
+				case '*':
+					return execute(node->op.paras[0]) * 
+						execute(node->op.paras[1]);
+				case '/':
+					return execute(node->op.paras[0]) / 
+						execute(node->op.paras[1]);
+				default:
+					printf("Unexpected opcode %d\n", node->op.op);
+					exit(0);
+			}
+	}
+	return 0;
+}
---
+++ Common/parser/eh_tree.c
@@ -0,0 +1,72 @@
+#include "eh.h"
+
+void free_node(ehnode_t *in) {
+	if(in == NULL)
+		return;
+	int i;
+	switch(in->type) {
+		case idnode_enum:
+			free(in->id.name);
+			break;
+		case connode_enum:
+			// nothing to free
+			break;
+		case opnode_enum:
+			for(i = 0; i < in->op.nparas; i++) {
+				free_node(in->op.paras[i]);
+			}
+			break;
+	}
+	free(in);
+}
+ehnode_t *get_constant(int value) {
+	ehnode_t *ret;
+	ret = Malloc(sizeof(ehnode_t));
+
+	ret->type = connode_enum;
+	ret->con.value = value;
+	
+	//printf("Returning constant %d\n", ret->con.value);
+	return ret;
+}
+ehnode_t *get_identifier(char *value) {
+	ehnode_t *ret;
+	ret = Malloc(sizeof(ehnode_t));
+	
+	ret->type = idnode_enum;
+	ret->id.name = Malloc(strlen(value) + 1);
+	strcpy(ret->id.name, value);
+	
+	//printf("Returning identifier %s\n", ret->id.name);
+	return ret;
+}
+ehnode_t *operate(int operation, int nparas, ...) {
+	va_list args;
+	ehnode_t *ret;
+	int i;
+	
+	ret = Malloc(sizeof(ehnode_t));
+	ret->op.paras = Malloc(nparas * sizeof(ehnode_t *));
+	
+	ret->type = opnode_enum;
+	ret->op.op = operation;
+	ret->op.nparas = nparas;
+	//printf("Adding operation %d with %d paras\n", ret->op.op, ret->op.nparas);
+	va_start(args, nparas);
+	for(i = 0; i < nparas; i++) {
+		ret->op.paras[i] = va_arg(args, ehnode_t *);
+		//printf("Para %d is of type %d\n", i, ret->op.paras[i]->type);
+	}
+	va_end(args);
+	
+	return ret;
+}
+
+void *Malloc(size_t size) {
+	void *ret;
+	ret = malloc(size);
+	if(ret == NULL) {
+		yyerror("Unable to allocate memory");
+		exit(1);
+	}
+}

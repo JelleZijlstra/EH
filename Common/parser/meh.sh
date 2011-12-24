c0955a014346a59048d63f4fb4ee06e470f55ed2
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2011-12-24 13:08:07 +0100

    Error if no argument

--- Common/parser/meh.sh
+++ Common/parser/meh.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Based on http://stackoverflow.com/questions/242538/unix-shell-script-find-out-which-directory-the-script-file-resides
+if [ ! $1 ]; then
+	echo 'No input file given'
+	exit
+fi
 PARDIR=$(dirname $0)
 if [ $PARDIR = '.' ]; then
 	PARDIR=$(pwd)

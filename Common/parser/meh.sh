9def33cc8241fdcd37b1ae68ce00f9fbefbf7ddb
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2011-12-24 13:06:32 +0100

    Add to meh.sh

--- Common/parser/meh.sh
+++ Common/parser/meh.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-PARDIR='/Users/jellezijlstra/Dropbox/git/Common/parser/'
-${PARDIR}ehc < $1.eh
-gcc -gstabs ${PARDIR}tmp.s -o $1
+# Based on http://stackoverflow.com/questions/242538/unix-shell-script-find-out-which-directory-the-script-file-resides
+PARDIR=$(dirname $0)
+if [ $PARDIR = '.' ]; then
+	PARDIR=$(pwd)
+fi
+${PARDIR}/ehc < $1.eh
+gcc -gstabs ${PARDIR}/tmp.s -o $1

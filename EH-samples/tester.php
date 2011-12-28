77045dc8794677b7fe2b4c9ffe6e8569a3455ad0
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2011-12-28 09:15:46 +0100

    Fix tester

--- EH-samples/tester.php
+++ EH-samples/tester.php
@@ -12,11 +12,11 @@ while($file = trim(fgets($testfiles))) {
 	echo "Testing $file...\n";
 	$expected = str_replace('.eh', '.expected', $file);
 	$output = str_replace('.eh', '.output', $file);
-	`/usr/bin/ehi '$file' > '$output' 2>> 'stderr.output'`;
-	`diff '$expected' '$output'`;
+	`/usr/bin/ehi $file > '$output' 2>> 'stderr.output'`;
+	echo `diff '$expected' '$output'`;
 	`rm '$output'`;
 }
 echo "Testing stderr output:\n";
-`diff stderr.expected stderr.output`;
+echo `diff stderr.expected stderr.output`;
 `rm stderr.output`;
 fclose($testfiles);

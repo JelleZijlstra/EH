3b5c1cb8227c01979e20ca9764fd9df1d3723c65
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2012-01-02 21:27:11 +0100

    Fix script

--- EH-parser/make_ehphp.sh
+++ EH-parser/make_ehphp.sh
@@ -2,12 +2,13 @@
 # Control the way ehphp is compiled on different OSes
 OS=`uname`
 
-case OS in
+case $OS in
 	'Darwin')
 		$1 -dynamiclib $2 -o ehphp.dylib $3
 		sudo cp ehphp.dylib /opt/local/lib/php/extensions/no-debug-non-zts-20090626/ehphp.dylib
 		;;
 	'Linux')
-		$1 -shared $2 -o ehphp.so $3
+		$1 -shared -o ehphp.so $3
+		sudo cp ehphp.so /usr/lib/php/modules/ehphp.so
 		;;
 esac

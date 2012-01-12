0225d0f443dc8e0f60452bf6d31585cb2125f9d9
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2012-01-11 21:53:45 -0500

    Add parse CLI

---
+++ Parse/parse.php
@@ -0,0 +1,20 @@
+#!/usr/bin/php
+<?php
+require_once(__DIR__ . "../Common/common.php");
+require_once(BPATH . "/Parse/parser.php");
+if(!isset($argv[1]))
+	exit(1);
+switch($argv[1]) {
+	case '-n': $citetype = 'normal';
+	case '-w':
+		if(!isset($argv[2])) mydie('No text to parse');
+		parse_wlist($argv[2]);
+		break;
+	case '-t':
+		if(!isset($argv[2])) mydie('No input file');
+		parse_wtext($argv[2]);
+		break;
+	default: parse_paper($argv[1]); break;
+}
+if($csvlist) $csvlist->saveifneeded();
+?>

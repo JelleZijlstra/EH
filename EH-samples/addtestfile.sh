72f8c031d4555d67f60bb5e9024164817e1120e3
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2012-01-31 16:01:37 -0500

    Remove separate output for stderr from tests

--- EH-samples/addtestfile.sh
+++ EH-samples/addtestfile.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 # Add a file to the EH test suite
-if [ ! $1 ]; then
-	echo 'No input file given'
-	exit
-fi
-infile=$1
-exp=${infile/.eh/.expected}
-echo "$infile" >> testfiles
-echo "@$infile:" >> stderr.expected
-/usr/bin/ehi $infile > $exp 2>> stderr.expected
-git add $infile
-git add $exp
+for infile in $@; do
+	exp=${infile/.eh/.expected}
+	echo "$infile" >> testfiles
+	/usr/bin/ehi $infile &> $exp
+	git add $infile
+	git add $exp
+done
--- EH-samples/tester.php
+++ EH-samples/tester.php
@@ -12,12 +12,8 @@ while($file = trim(fgets($testfiles))) {
 	echo "Testing $file...\n";
 	$expected = str_replace('.eh', '.expected', $file);
 	$output = str_replace('.eh', '.output', $file);
-	`echo '@$file:' >> stderr.output`;
-	`/usr/bin/ehi $file > '$output' 2>> 'stderr.output'`;
+	`/usr/bin/ehi $file &> '$output'`;
 	echo `diff '$expected' '$output'`;
 	`rm '$output'`;
 }
-echo "Testing stderr output:\n";
-echo `diff stderr.expected stderr.output`;
-`rm stderr.output`;
 fclose($testfiles);

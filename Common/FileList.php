84a13269255d46a67689c888dc1f3434d7debd7b
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2012-01-31 09:56:22 -0500

    It doesn't look like c is actually used anywhere outside FileList and its implementers, but we'll see whether this breaks anything

--- Common/FileList.php
+++ Common/FileList.php
@@ -22,7 +22,7 @@ abstract class FileList extends ExecuteHandler {
 	 * modify this directly.
 	 */
 	private $needsave = false;
-	public $c; //array of children
+	protected $c; //array of children
 	protected $labels; // first line of CSV file
 	protected static $fileloc; // where the file lives that we get our list from
 	protected static $childclass; // name of the class that children need to be a member of

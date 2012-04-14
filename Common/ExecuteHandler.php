dadfc59c1d6136efbd9bc41974dd05b601b24912
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2012-04-13 20:26:45 -0400

    Both are long done

--- Common/ExecuteHandler.php
+++ Common/ExecuteHandler.php
@@ -10,8 +10,6 @@
  */
 define('PROCESS_PARAS_ERROR_FOUND', false);
 require_once(BPATH . "/Common/EHException.php");
-// TODO: more effectively ignore Ctrl+P and stuff like that.
-// Fix function definitions in exec_file. Currently, they just do random stuff when executed outside the exec_file context.
 class ExecuteHandler extends EHICore {
 	/* Class constants */
 	const EXECUTE_NEXT = 0x0; // execute says: go on with next

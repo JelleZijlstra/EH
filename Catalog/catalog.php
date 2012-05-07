e33984d30d086f0c130a291da8dc890748021d84
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2012-05-07 00:52:51 -0400

    Don't use short tags

--- Catalog/catalog.php
+++ Catalog/catalog.php
@@ -1,5 +1,5 @@
 #!/usr/bin/php
-<?
+<?php
 require_once(__DIR__ . '/../Common/common.php');
 if(!isset($argv[1])) {
 	ArticleList::singleton()->cli();
--- Common/common.php
+++ Common/common.php
@@ -1,4 +1,4 @@
-<?
+<?php
 /*
  * common.php
  * Jelle Zijlstra, March 2011

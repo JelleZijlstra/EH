cc45e807e1ad4b09077a7623ba9e2db80a8403a1
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2012-05-29 15:17:59 +0200

    Add methods, fix properties

--- Catalog/Author.php
+++ Catalog/Author.php
@@ -2,24 +2,52 @@
 class Author extends SqlListEntry {
 	protected $id;
 	
-	protected $firstnames;
+	protected $first_names;
 	
 	protected $name;
 	
+	protected $suffix;
+	
 	protected static $Author_commands = array(
 	);
+	
+	public function initials() {
+		if(strpos($this->first_names, ' ') !== false) {
+			return array_map(function($name) {
+				return $name[0] . '.';
+			}, explode(' ', $this->first_names));
+		} else {
+			return $this->first_names;
+		}
+	}
+	
+	public function toArray() {
+		return array($this->initials(), $this->name, $this->suffix);
+	}
 	
 	protected static function fillFields() {
 		return array(
 			'id' => new SqlProperty(array(
 				'name' => 'id',
 				'type' => SqlProperty::ID)),
-			'firstnames' => new SqlProperty(array(
-				'name' => 'firstnames',
-				'type' => SqlProperty::STRING)),
+			'first_names' => new SqlProperty(array(
+				'name' => 'first_names',
+				'type' => SqlProperty::STRING,
+				'validator' => function($in) {
+					return ($in === '') 
+						|| preg_match('/^(\w+( \w+)*|(\w\w?\.)+)$/', $in);
+				})),
 			'name' => new SqlProperty(array(
 				'name' => 'name',
 				'type' => SqlProperty::STRING)),
+			'suffix' => new SqlProperty(array(
+				'name' => 'suffix',
+				'type' => SqlProperty::STRING
+				'validator' => function($in) {
+					return ($in === 'Jr.') 
+						|| ($in === 'Sr.')
+						|| preg_match('/^[IV]+$/', $in);
+				})),
 		);
 	}
 }

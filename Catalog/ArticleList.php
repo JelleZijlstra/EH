d419ee670d27eb6a9a64e62766e3df8284d8aba9
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2012-05-29 08:53:33 +0200

    Fix a few bugs

--- Catalog/ArticleList.php
+++ Catalog/ArticleList.php
@@ -4,6 +4,12 @@ require_once(__DIR__ . '/../Common/common.php');
 class ArticleList extends SqlContainerList {
 	use CommonArticleList;
 	
+	static $childClass = 'Article';
+
+	protected function __construct(array $commands = array()) {
+		parent::__construct(self::$ArticleList_commands);
+	}
+	
 	public function addRedirect(array $paras) {
 		//TODO
 	}

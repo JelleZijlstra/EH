e152a40c7827c42f69a419bf53e6372c517687ed
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2012-06-03 16:07:49 +0200

    Another build

--- EH-parser/make_ehphp.sh
+++ EH-parser/make_ehphp.sh
@@ -6,6 +6,7 @@ case $OS in
 	'Darwin')
 		$1 -dynamiclib $2 -o ehphp.dylib $3
 		sudo cp ehphp.dylib /opt/local/lib/php54/extensions/no-debug-non-zts-20100525/ehphp.dylib
+		sudo cp ehphp.dylib /opt/local/lib/php54/extensions/debug-non-zts-20100525/ehphp.dylib
 		;;
 	'Linux')
 		$1 -shared -o ehphp.so $3

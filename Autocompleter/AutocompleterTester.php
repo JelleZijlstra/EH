fe51e61cc37dc8d01432cb08fa7900e1bc8deeaf
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2012-06-14 14:32:13 +0200

    Add profiling

--- Autocompleter/AutocompleterTester.php
+++ Autocompleter/AutocompleterTester.php
@@ -29,5 +29,38 @@ class AutocompleterTester {
 			self::test($class);
 		}
 	}
+	
+	private static function profile(/* string */ $class, array $data) {
+		echo 'Profiling class: ' . $class . PHP_EOL;
+		$memoryBefore = memory_get_usage();
+		$timeBefore = microtime(true);
+		$ac = new $class($data);
+		$timeAfter = microtime(true);
+		$memoryAfter = memory_get_usage();
+		echo 'Time taken for object creation: ' 
+			. round($timeAfter - $timeBefore, 6) . ' s' . PHP_EOL;
+		echo 'Memory taken: ' . 
+			Sanitizer::intToBytes($memoryAfter - $memoryBefore) . PHP_EOL;
+		
+		$timeBefore = microtime(true);
+		for($i = 0; $i < 100; $i++) {
+			$ac->lookup('Agat');
+			$ac->lookup('Oryz');
+			$ac->lookup('Sivalad');
+			$ac->lookup('A');
+		}
+		$timeAfter = microtime(true);
+		echo 'Time taken for lookup: '
+			. round($timeAfter - $timeBefore, 6) . ' s' . PHP_EOL;
+		echo PHP_EOL;
+	}
+	
+	public static function runProfiling() {
+		$names = CsvArticleList::singleton()->getNameArray();
+		foreach(self::$classes as $class) {
+			self::profile($class, $names);
+		}		
+	}
 }
 AutocompleterTester::runTests();
+AutocompleterTester::runProfiling();

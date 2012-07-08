bfb88134403dd141eefa3fa3144fd62329dc419a
Jelle Sjoerd Zijlstra <jelle.zijlstra@college.harvard.edu>
2012-07-07 21:25:25 -0700

    Fix warnings in g++ 4.5

--- EH-parser/eh_gc.h
+++ EH-parser/eh_gc.h
@@ -37,6 +37,7 @@ private:
 		return this->pointer;
 	}
 	
+	class dummy_class {};
 public:
 	static refcount_ptr<T> clone(refcount_ptr<T> in) {
 		refcount_ptr<T> out;
@@ -60,9 +61,8 @@ public:
 		}
 	}
 	
-	// TODO: fix warnings in g++4.5 about converting NULL to int. One solution is to have a private "dummy_class" here and have a constructor that takes a dummy_class* argument (https://groups.google.com/forum/?fromgroups#!topic/comp.lang.c++/4pIZb6Glxa4). However, that creates an ambiguity with the current T* constructor. That will no longer be an issue when we can kill that constructor (i.e., if we no longer have ehretval_t * flying around in the AST).
-	refcount_ptr(int in) {
-		if(in == 0) {
+	refcount_ptr(dummy_class *in) {
+		if(in == NULL) {
 			this->pointer = NULL;
 		} else {
 			throw 0;

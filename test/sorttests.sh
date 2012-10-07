a1a67c359a4d1a838a11a898958b1cad8ee6d436
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2012-10-07 13:50:50 -0400

    Resort

--- test/sorttests.sh
+++ test/sorttests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Sort the tests in the testfiles file
 cp testfiles testfiles-tmp
 sort testfiles-tmp | uniq > testfiles-sorted
 cat -s testfiles-sorted > testfiles
+rm testfiles-sorted testfiles-tmp
